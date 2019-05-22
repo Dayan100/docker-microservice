cbd5f5430c861cbeedc93900f7d5025f4b49dd3e
harishanumandla817 <harishanumandla817@gmail.com>
2019-05-22 15:28:34 +0530

    added comments

--- src/com/ness/GreetingController.java
+++ src/com/ness/GreetingController.java
@@ -2,14 +2,14 @@ package com.ness;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//added comment 
 @RestController
 public class GreetingController {
 
         @RequestMapping("/")
         public String greeting()
         {
-            return " <H1>Hello chandra</H1>";
+            return " <H1>Hello S&P</H1>";
         }
 
 }
